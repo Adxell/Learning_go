19feb14242901ce957740ee768da817be89b3b61
Adxell <adxell02@gmail.com>
2021-11-28 21:21:23 -0500

    update handler person

--- claseCrud/handler/person.go
+++ claseCrud/handler/person.go
@@ -1,5 +1,5 @@
 package handler 
-
+import "Learning_go/claseCrud/model"
 type person struct {
 	storage Storage
 }
@@ -15,4 +15,25 @@ func (p *person) create(w http.ResponseWriter, r *http.Request){
 		w.Write([]byte(`{"message_type":"error", "message": "Method no default"}`))
 		return
 	}
+
+	data:=model.Person{}
+	err:=json.newDecoder(r.Body).Decode(&data)
+	if err !=nil{
+		w.Header().Set("Content-Type", "appplication/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"message_type":"error", "message": "Data  does not correct"}`))
+		return
+	}
+
+	err = p.storage.Create(&data)
+	if err !=nil{
+		w.Header().Set("Content-Type", "appplication/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"message_type":"error", "message": "Problen with server"}`))
+		return
+	}
+
+	w.Header().Set("Content-Type", "appplication/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`{"message_type":"successful", "message": "Person Created"}`))
 }
